3d7ef1a71d37997e3d8783185e5f4f66b6b1a77a
Velibor Zeli <velibor@mech.kth.se>
2019-05-31 10:38:12 +0200

    Removes trailing whitespace from the mail content
    Whitespace characters at the end of each line are annoying and unwanted.
    rstrip_whitespace is a new function that removes the trailing whitespace
    characters and cleans the mail content before assigning it to content
    attribute.

--- src/emailbrush.py
+++ src/emailbrush.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
 from structure import Paragraph
 
+
 def read_email(email_path):
     with open(email_path, 'r') as f:
         text = f.read()
@@ -9,6 +10,7 @@ def read_email(email_path):
     return OrderedDict(
         {i: Paragraph(paragraph) for i, paragraph in enumerate(paragraphs)}
     )
+
 
 if __name__ == '__main__':
     email_file = 'input_file.txt'
--- src/structure.py
+++ src/structure.py
@@ -2,23 +2,35 @@ import textwrap
 from functools import reduce
 from operator import add
 
+
 DEFAULT_WIDTH = 58
 
 class Paragraph:
 
     def __init__(self, content):
-        self.content = content
+        self.content = self.rstrip_whitespace(content)
         self.type = None
 
-    def __str__(self):
-        return '{}'.format(self.content)
-
-    def __repr__(self):
-        return f'Paragraph({self.content!r})'
-
     def wrap(self, width=DEFAULT_WIDTH):
-        """Change self.content by putting \n at width distance."""
+        """Change self.content by putting \\n at width distance."""
         raw_content = self.content.replace('\n', ' ')
         wrapped_lines = textwrap.wrap(raw_content, width)
         wrapped_lines = [line + '\n' for line in wrapped_lines]
         self.content = reduce(add, wrapped_lines)
+        return None
+
+    @staticmethod
+    def rstrip_whitespace(content):
+        """Remove a single/multiple whitespace character/s from the end
+        of every self.content line.
+        """
+        while ' \n' in content or '\t\n' in content:
+            content = content.replace(' \n', '\n')
+            content = content.replace('\t\n', '\n')
+        return content
+
+    def __str__(self):
+        return f'{self.content}'
+
+    def __repr__(self):
+        return f'Paragraph({self.content!r})'
