a1eaee263bf78a8da92dbd842ab4c8973e406b7e
Velibor Zeli <velibor@mech.kth.se>
2019-06-06 23:56:49 +0200

    Make Email class inherit from collections.UserList
    This will simplify the usage of the Email class since this class is the
    "interface" class that should be the most user-friendly.

--- src/structure.py
+++ src/structure.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import collections
 import textwrap
 from functools import reduce
 from operator import add
@@ -6,13 +7,15 @@ from operator import add
 
 HARD_RETURN = '\\hardreturn'
 
-def has_hard_return(text):
-    return True if HARD_RETURN in text else False
-
-
-def linter(repr_text):
+def clean_text(repr_text):
     """Remove multiple or single whitespace except for the leading ones."""
     return ' '.join(repr_text.split())
+
+
+def split_into_subparagraphs(repr_text):
+    parts = repr_text.split(HARD_RETURN)
+    subparagraphs = [_SubParagraph(part) for part in parts]
+    return subparagraphs
 
 
 class Paragraph:
@@ -20,8 +23,8 @@ class Paragraph:
 
     def __init__(self, repr_text):
         self.repr_text = repr_text
-        self._subparagraphs = self.split_paragraph(repr_text)
-        self.__class__.count += 1
+        self._subparagraphs = split_into_subparagraphs(repr_text)
+        self.increment_count()
 
     def __str__(self):
         return '\n'.join(spar.text for spar in self._subparagraphs)
@@ -29,23 +32,21 @@ class Paragraph:
     def __repr__(self):
         return f'Paragraph({self.repr_text!r})'
 
-    @staticmethod
-    def split_paragraph(repr_text):
-        if has_hard_return(repr_text):
-            parts = repr_text.split(HARD_RETURN)
-            subparagraphs = [_SubParagraph(part.strip()) for part in parts]
-        else:
-            subparagraphs = [_SubParagraph(repr_text)]
-        return subparagraphs
+    @classmethod
+    def get_count(cls):
+        return cls.count
+
+    @classmethod
+    def increment_count(cls):
+        cls.count += 1
 
 
-class _SubParagraph:
-    count = 0
+class _SubParagraph(Paragraph):
 
     def __init__(self, repr_text):
         self.repr_text = repr_text
-        self.text = linter(repr_text)
-        self.__class__.count += 1
+        self.text = clean_text(repr_text)
+        self.increment_count()
 
     def __str__(self):
         return self.text
@@ -58,11 +59,14 @@ class _SubParagraph:
         self.text = '\n'.join(line for line in wrapped_lines)
 
 
-class Email:
+class Email(collections.UserList):
 
     def __init__(self, paragraphs, add_signature=False):
         self._paragraphs = paragraphs
         self.add_signature = add_signature
+
+    def __getitem__(self, value):
+        return self._paragraphs[value]
 
     def __repr__(self):
         return (
@@ -71,6 +75,6 @@ class Email:
         )
 
     def wrap(self, width=56, **kwargs):
-        for paragraph in self._paragraphs:
+        for paragraph in self:
             for subparagraph in paragraph._subparagraphs:
                 subparagraph._wrap(width, **kwargs)
