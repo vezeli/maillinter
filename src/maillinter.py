341e6eb6354c1fd4ec8017ed1b94040fe3329f71
Velibor Zeli <velibor@mech.kth.se>
2019-06-09 23:31:21 +0200

    Use maillinter.py as a command line program
    Using argparse Python3 standard library maillinter.py is now called as a
    command line program. Standard functionality is invoked through setting
    of the programs positional and optional arguments.

--- src/maillinter.py
+++ src/maillinter.py
@@ -1,15 +1,46 @@
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import argparse
+
 import _base
 
 
-def read_email(email_path):
-    with open(email_path, 'r') as f:
+def main():
+    with open(args.input, 'r') as f:
         text = f.read()
     paragraphs = text.split('\n\n')
     paragraphs = [_base.Paragraph(text) for text in paragraphs]
 
-    return _base.Email(paragraphs)
+    mail = _base.Email(paragraphs)
+    mail.wrap(args.wrap)
+
+    if args.output:
+        write(args.output, mail)
+    else:
+        for paragraph in mail:
+            print('')
+            for subparagraph in paragraph.subparagraphs:
+                print(subparagraph)
+
+
+def write(out_file, email_obj):
+    with open(out_file, 'w') as f:
+        for paragraph in email_obj:
+            f.write('\n')
+            for subparagraph in paragraph.subparagraphs:
+                f.write(subparagraph.text)
+                f.write('\n')
+
+
+description_msg = 'Lint the whitespace and wrap the content of an email.'
+parser = argparse.ArgumentParser(description=description_msg)
+parser.add_argument('input', type=str, help='input file')
+parser.add_argument('-o', dest='output', type=str, help='output file')
+parser.add_argument(
+    '-w', dest='wrap', type=int, default=56, help='wrapped line lenght'
+)
+args = parser.parse_args()
+
 
 if __name__ == '__main__':
-    email_file = 'input_file.txt'
-    mail = read_email(email_file)
+    main()
